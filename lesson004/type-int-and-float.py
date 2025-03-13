37968cfe66a7f31bf9da393fb84c4c5f143e8190
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-13 06:30:01 -0300

    add: lesson004 - type int & float

---
+++ lesson004/type-int-and-float.py
@@ -0,0 +1,14 @@
+# Types int & float
+# int -> Integer
+# print(11) # int
+# print(-11) # int
+# print(0)
+
+# float -> Floating number
+# print(1.1, 10.11)
+# print(0.0, -1.5)
+
+# Type function retrieves its args type
+print(type('Name'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
