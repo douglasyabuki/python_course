51fd320a99f556f5ea29b0ab0125802ce27661c3
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-13 06:40:37 -0300

    add: lesson006 - type conversion & coercion

---
+++ lesson006/type-conversion-and-coercion.py
@@ -0,0 +1,9 @@
+# Type conversion, coercion
+# Type conversion, typecasting, coercion
+# is the act of converting one type into another.
+# Immutable and primitive types:
+# str, int, float, bool
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
