b29c6dda6ba4e41f8e2d103ecd1f5a4093c817f0
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-13 06:43:07 -0300

    add: lesson007 - python variables

---
+++ lesson007/python-variables.py
@@ -0,0 +1,18 @@
+# Variables are used to store something in the computer's memory.
+# PEP8: start variable names with lowercase letters; you can use
+# numbers and underscores (_).
+# The = sign is the assignment operator. It is used to
+# assign a value to a name (variable).
+# Usage: variable_name = expression
+
+# full_name = 'Name Lastname'
+# sum_two_plus_two = 2 + 2
+# int_one = bool('1')
+# print(int_one, type(int_one))
+# print(full_name, sum_two_plus_two)
+
+name = 'Name'
+age = 17
+is_adult = age >= 18
+print('Name:', name, 'Age:', age)
+print('Is an adult?', is_adult)
