011f96f458bd9edbc764c2718728bc698e3f6603
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-13 06:38:26 -0300

    add: lesson005 - type boolean

---
+++ lesson005/type-boolean.py
@@ -0,0 +1,13 @@
+# Boolean Type
+# When questioning something in a program,
+# there are only two possible answers:
+# yes (True) or no (False).
+# There are several operators for "questioning".
+# Among them is ==, which is a logical operator that
+# checks if one value is equal to another.
+print(10 == 10)  # Yes => True (True)
+print(10 == 11)  # No => False (False)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
