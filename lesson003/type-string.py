72e2a6601d4891ee02c9d84654a3ff9a8c6e6548
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-13 05:56:30 -0300

    add: lesson003 - type string

---
+++ lesson003/type-string.py
@@ -0,0 +1,21 @@
+"""
+DocString
+Python = Programming Language
+Strong / Dynamic typing
+Strings are texts inside quotations
+"""
+print(1234)
+
+# Single quotes
+print('Name lastname')
+print(1, 'Name "lastname"')
+
+# Double quotes
+print("Name lastname")
+print(2, "Name 'lastname'")
+
+# Escape
+print("Name \"lastname\"")
+
+# r
+print(r"Name \"lastname\"")
