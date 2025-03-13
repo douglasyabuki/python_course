4653c403c85a40d31832cc0fc0b3e9486d482965
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-13 07:17:35 -0300

    add: lesson012 - math operators exercise

---
+++ lesson012/math-operators-exercise.py
@@ -0,0 +1,12 @@
+name = 'Name Lastname'
+height = 1.80
+weight = 95
+bmi = weight / height ** 2  # BMI (Body Mass Index)
+
+print(name, 'is', height, 'meters tall,')
+print('weighs', weight, 'kilograms, and his BMI is')
+print(bmi)
+
+# Name Lastname is 1.80 meters tall,
+# weighs 95 kilograms, and his BMI is
+# 29.320987654320987
