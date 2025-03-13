d87a04372907ddf8a770ebc97ea0c1d368cb9ec1
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-13 07:07:44 -0300

    add: lesson008 - python variables exercise

---
+++ lesson008/python-variables-exercise.py
@@ -0,0 +1,13 @@
+name = 'Name'
+surname = 'Surname'
+age = 18
+year_of_birth = 2022 - age
+is_adult = age >= 18
+height_meters = 1.80
+
+print('Name:', name)
+print('Surname:', surname)
+print('Age:', age)
+print('Year of birth:', year_of_birth)
+print('Is an adult?', is_adult)
+print('Height in meters:', height_meters)
