51cc99cc0dd04f20da6eeeb2f66d2e07c6338ecd
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-14 05:15:33 -0300

    add: lesson018 - vscode debugger & python interpretor

---
+++ lesson018/vscode-debugger-and-python-interpretor.py
@@ -0,0 +1,25 @@
+# Run and Debug > create a launch.json file > Python Debugger @link {https://www.udemy.com/course/python-3-do-zero-ao-avancado/learn/lecture/34554908#overview}
+# if / elif      / else
+# if / else if / else
+
+condition1 = True
+condition2 = True
+condition3 = True
+condition4 = True
+
+if condition1:
+    print('Code for condition 1')
+    print('Code for condition 1')
+elif condition2:
+    print('Code for condition 2')
+elif condition3:
+    print('Code for condition 3')
+elif condition4:
+    print('Code for condition 4')
+else:
+    print('No condition was met.')
+
+if 10 == 10:
+    print('Another if')
+
+print('Outside the if')
