b7af0644934167dbf1d5bce7abd266d9aea3a587
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-14 04:55:14 -0300

    add: lesson016 - conditionals

---
+++ lesson016/conditionals.py
@@ -0,0 +1,14 @@
+# if / elif      / else
+# if / else if / else
+entry = input('Do you want to "enter" or "exit"? ')
+
+if entry == 'enter':
+    print('You have entered the system')
+
+    print(12341234)
+elif entry == 'exit':
+    print('You have exited the system')
+else:
+    print('You did not type either enter or exit.')
+
+print('OUTSIDE THE BLOCKS')
