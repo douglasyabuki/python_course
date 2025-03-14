f906486804965a4d738f2e544d501fb38eef79f0
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-14 05:34:21 -0300

    add: lesson027 - string slicing

---
+++ lesson027/string-slicing.py
@@ -0,0 +1,11 @@
+"""
+String slicing
+ 012345678
+ Hello World
+-987654321
+Slicing [start:end:step] [::]
+Note: The len function returns the number 
+of characters in the string.
+"""
+variable = 'Hello World'
+print(variable[::-1])  # Reverses the string
