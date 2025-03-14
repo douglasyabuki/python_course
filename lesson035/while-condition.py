b42d257d58fb9ab555afd0526df15f19d14d9db7
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-14 06:16:37 -0300

    add: lesson035 - while condition

---
+++ lesson035/while-condition.py
@@ -0,0 +1,13 @@
+"""
+Repetitions
+while (while loop)
+Executes an action while a condition is true
+Infinite loop -> When a code has no end
+"""
+counter = 0
+
+while counter <= 10:
+    counter = counter + 1
+    print(counter)
+
+print('Finished')
