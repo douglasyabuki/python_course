14ce7311b06125c9c0b969ad751d05d45b6b9de7
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-14 05:22:32 -0300

    add: lesson023 - logical operator not

---
+++ lesson023/logical-operator-not.py
@@ -0,0 +1,9 @@
+# Logical operator "not"
+# Used to invert expressions
+# not True = False
+# not False = True
+
+# password = input('Password: ')
+
+print(not True)  # False
+print(not False)  # True
