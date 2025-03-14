99d6cc844272a4397b37a215100031274cfe1a84
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-14 06:06:07 -0300

    add: lesson033 - built-in types, docs, immutable type & string methods

---
+++ lesson033/built-in-types-and-docs.py
@@ -0,0 +1,9 @@
+"""
+https://docs.python.org/3/library/stdtypes.html
+Immutable types we have seen: str, int, float, bool
+"""
+string = '1000'
+# another_variable = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(another_variable)
+print(string.zfill(10))  # Pads the string with leading zeros up to 10 characters
