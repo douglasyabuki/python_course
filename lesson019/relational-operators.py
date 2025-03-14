e5f354f55f481ea6d9d2d0a8beb0542aafab0452
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-14 05:16:36 -0300

    add: lesson019 - relational operators

---
+++ lesson019/relational-operators.py
@@ -0,0 +1,17 @@
+"""
+Comparison (relational) operators
+OP      Meaning             Example (True)
+>       greater than        2 > 1
+>=      greater or equal    2 >= 2
+<       less than           1 < 2
+<=      less or equal       2 <= 2
+==      equal               'a' == 'a'
+!=      not equal           'a' != 'b'
+"""
+greater = 2 > 1
+greater_or_equal = 2 >= 2
+less = 1 < 2
+less_or_equal = 2 <= 2
+equal = 'a' == 'a'
+not_equal = 'a' != 'b'
+print('Look at my print here')
