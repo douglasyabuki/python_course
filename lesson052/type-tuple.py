a195ed5457ceca87d1fe89187057dede2b783853
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-15 05:04:30 -0300

    add: lesson052 - type tuple

---
+++ lesson052/type-tuple.py
@@ -0,0 +1,8 @@
+"""
+Tuple type - An immutable list
+"""
+names = ('Name', 'John', 'Doe')
+# names = tuple(names)  # Converts a list to a tuple
+# names = list(names)   # Converts a tuple to a list
+print(names[-1])  # Prints the last element ('Doe')
+print(names)  # Prints the entire tuple
