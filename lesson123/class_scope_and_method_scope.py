211868b5f629450309cf628540d646268163963e
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-18 05:09:04 -0300

    add: lesson123 - class scope & method scope

---
+++ lesson123/class_scope_and_method_scope.py
@@ -0,0 +1,20 @@
+# Class scope and method scope
+class Animal:
+    # name = 'Lion'
+
+    def __init__(self, name):
+        self.name = name
+
+        variable = 'value'
+        print(variable)
+
+    def eating(self, food):
+        return f'{self.name} is eating {food}'
+
+    def execute(self, *args, **kwargs):
+        return self.eating(*args, **kwargs)
+
+
+lion = Animal(name='Lion')
+print(lion.name)
+print(lion.execute('apple'))
