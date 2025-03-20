6e466bd8395503bd584db364b643498406a07d83
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-20 05:27:48 -0300

    add: lesson175 - json concepts

---
+++ lesson175/json_concepts.py
@@ -0,0 +1,24 @@
+# What is JSON - JavaScript Object Notation
+# JSON - JavaScript Object Notation (.json extension)
+# It is a data structure that allows serialization
+# of objects into plain text, facilitating data transmission
+# over networks, web APIs, or other communication channels.
+# JSON supports the following data types:
+# Numbers: integers or floating-point numbers, e.g., 42 or 3.14
+# Strings: character sequences, e.g., "Hello, World!" or "12345"
+#   Strings must be enclosed in double quotes
+# Booleans: true or false values
+# Arrays: ordered lists of values, e.g., [1, 2, 3] or
+#   ["Hi", "Hello", "Good morning"]
+# Objects: collections of key/value pairs, e.g., {"name": "John", "age": 30}
+# null: a special value indicating the absence of a value
+#
+# When converting from Python to JSON:
+# Python        JSON
+# dict          object
+# list, tuple   array
+# str           string
+# int, float    number
+# True          true
+# False         false
+# None          null
