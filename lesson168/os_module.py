bd4d4bbd111aa6ccc137709937b558472c962b09
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-20 05:11:04 -0300

    add: lesson168 - os module

---
+++ lesson168/os_module.py
@@ -0,0 +1,20 @@
+# The os module for interacting with the system
+# Doc: https://docs.python.org/3/library/os.html
+# The `os` module provides functions to interact with the operating system.
+# For example, the os.path module contains functions to work with file paths,
+# and the os.listdir() function can be used to list files in a directory.
+# The os.system() method allows you to execute system commands
+# from your Python code.
+# Windows 11 (PowerShell), Linux, Mac = clear
+# Windows (old, cmd) = cls
+import os
+
+os.system('clear')
+os.system('echo "Hello world"')
+
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
