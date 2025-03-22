c6adaccafb6148d21694415fcbfba7a326c70b9b
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-22 06:37:15 -0300

    add: lesson191 - http req & res

---
+++ lesson191/http_req_res.py
@@ -0,0 +1,15 @@
+# Using requests for HTTP requests
+# Tutorial -> https://youtu.be/Qd8JT0bnJGs
+import requests
+
+# Default ports:
+# http:// -> 80
+# https:// -> 443
+url = 'http://localhost:8000/'
+response = requests.get(url)
+
+print(response.status_code)
+# print(response.headers)
+# print(response.content)
+# print(response.json())
+print(response.text)
