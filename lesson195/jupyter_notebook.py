5975ac1c56071f4b04d29bc9dc2c4833669eae85
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-22 07:59:13 -0300

    add: lesson195 - jupyter notebook

---
+++ lesson195/jupyter_notebook.py
@@ -0,0 +1,10 @@
+# Jupyter Notebook - Installation and testing
+
+# This line creates a variable `a`
+a = 2
+
+# This line sets variable `b` to twice the value of `a`
+b = a * 2  # 4
+
+# This line prints the sum of `a` and `b`
+print(a + b)  # 6
